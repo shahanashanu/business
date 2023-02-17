41e7612a693b80e1d7b005743e76e96901d3441f
EC2 Default User <ec2-user@ip-172-31-3-242.eu-west-2.compute.internal>
2023-02-17 20:15:26 +0000

    mysql backup

--- sample.sql
+++ sample.sql
@@ -68,6 +68,160 @@ INSERT INTO `answer_choice` VALUES (1,1,'yes',0),(2,1,'no',1);
 UNLOCK TABLES;
 
 --
+-- Table structure for table `audit`
+--
+
+DROP TABLE IF EXISTS `audit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit` (
+  `audit_id` int NOT NULL AUTO_INCREMENT,
+  `company_id` int DEFAULT NULL,
+  `term_id` int DEFAULT NULL,
+  `audit_date` date DEFAULT NULL,
+  `created_date` date DEFAULT (curdate()),
+  `auditor_id` int DEFAULT NULL,
+  `file_attachment_id` int DEFAULT NULL,
+  `audit_status_id` int DEFAULT NULL,
+  PRIMARY KEY (`audit_id`),
+  KEY `company_id` (`company_id`),
+  KEY `term_id` (`term_id`),
+  KEY `auditor_id` (`auditor_id`),
+  KEY `file_attachment_id` (`file_attachment_id`),
+  KEY `audit_status_id` (`audit_status_id`),
+  CONSTRAINT `audit_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company` (`company_id`),
+  CONSTRAINT `audit_ibfk_2` FOREIGN KEY (`term_id`) REFERENCES `term` (`term_id`),
+  CONSTRAINT `audit_ibfk_3` FOREIGN KEY (`auditor_id`) REFERENCES `person` (`person_id`),
+  CONSTRAINT `audit_ibfk_4` FOREIGN KEY (`file_attachment_id`) REFERENCES `file_attachment` (`file_attachment_id`),
+  CONSTRAINT `audit_ibfk_5` FOREIGN KEY (`audit_status_id`) REFERENCES `audit_status` (`audit_status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `audit`
+--
+
+LOCK TABLES `audit` WRITE;
+/*!40000 ALTER TABLE `audit` DISABLE KEYS */;
+INSERT INTO `audit` VALUES (1,1,1,NULL,'2023-02-17',2,1,3),(2,2,2,NULL,'2023-02-17',3,1,1);
+/*!40000 ALTER TABLE `audit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `audit_answer`
+--
+
+DROP TABLE IF EXISTS `audit_answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit_answer` (
+  `audit_answer_id` int NOT NULL AUTO_INCREMENT,
+  `audit_questions_id` int DEFAULT NULL,
+  `answer_choice_id` int DEFAULT NULL,
+  PRIMARY KEY (`audit_answer_id`),
+  KEY `audit_questions_id` (`audit_questions_id`),
+  KEY `answer_choice_id` (`answer_choice_id`),
+  CONSTRAINT `audit_answer_ibfk_1` FOREIGN KEY (`audit_questions_id`) REFERENCES `audit_questions` (`audit_questions_id`),
+  CONSTRAINT `audit_answer_ibfk_2` FOREIGN KEY (`answer_choice_id`) REFERENCES `answer_choice` (`answer_choice_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `audit_answer`
+--
+
+LOCK TABLES `audit_answer` WRITE;
+/*!40000 ALTER TABLE `audit_answer` DISABLE KEYS */;
+INSERT INTO `audit_answer` VALUES (1,1,1);
+/*!40000 ALTER TABLE `audit_answer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `audit_auditee`
+--
+
+DROP TABLE IF EXISTS `audit_auditee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit_auditee` (
+  `audit_auditee_id` int NOT NULL AUTO_INCREMENT,
+  `audit_id` int DEFAULT NULL,
+  `auditee_id` int DEFAULT NULL,
+  PRIMARY KEY (`audit_auditee_id`),
+  KEY `audit_id` (`audit_id`),
+  KEY `auditee_id` (`auditee_id`),
+  CONSTRAINT `audit_auditee_ibfk_1` FOREIGN KEY (`audit_id`) REFERENCES `audit` (`audit_id`),
+  CONSTRAINT `audit_auditee_ibfk_2` FOREIGN KEY (`auditee_id`) REFERENCES `person` (`person_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `audit_auditee`
+--
+
+LOCK TABLES `audit_auditee` WRITE;
+/*!40000 ALTER TABLE `audit_auditee` DISABLE KEYS */;
+INSERT INTO `audit_auditee` VALUES (1,1,2),(2,1,3);
+/*!40000 ALTER TABLE `audit_auditee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `audit_questions`
+--
+
+DROP TABLE IF EXISTS `audit_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit_questions` (
+  `audit_questions_id` int NOT NULL AUTO_INCREMENT,
+  `question_id` int DEFAULT NULL,
+  `audit_id` int DEFAULT NULL,
+  PRIMARY KEY (`audit_questions_id`),
+  KEY `question_id` (`question_id`),
+  KEY `audit_id` (`audit_id`),
+  CONSTRAINT `audit_questions_ibfk_1` FOREIGN KEY (`question_id`) REFERENCES `question` (`question_id`),
+  CONSTRAINT `audit_questions_ibfk_2` FOREIGN KEY (`audit_id`) REFERENCES `audit` (`audit_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `audit_questions`
+--
+
+LOCK TABLES `audit_questions` WRITE;
+/*!40000 ALTER TABLE `audit_questions` DISABLE KEYS */;
+INSERT INTO `audit_questions` VALUES (1,2,1),(2,2,2);
+/*!40000 ALTER TABLE `audit_questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `audit_results`
+--
+
+DROP TABLE IF EXISTS `audit_results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `audit_results` (
+  `audit_results_id` int NOT NULL AUTO_INCREMENT,
+  `audit_id` int DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  PRIMARY KEY (`audit_results_id`),
+  KEY `audit_id` (`audit_id`),
+  CONSTRAINT `audit_results_ibfk_1` FOREIGN KEY (`audit_id`) REFERENCES `audit` (`audit_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `audit_results`
+--
+
+LOCK TABLES `audit_results` WRITE;
+/*!40000 ALTER TABLE `audit_results` DISABLE KEYS */;
+INSERT INTO `audit_results` VALUES (1,1,0);
+/*!40000 ALTER TABLE `audit_results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `audit_status`
 --
 
@@ -388,4 +542,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-15 18:15:19
+-- Dump completed on 2023-02-17 20:13:48
